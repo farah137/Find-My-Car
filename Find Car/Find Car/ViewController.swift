d743e7289db5441ad75cb5480030bef88e9a7563
User <user@Users-MacBook-Pro.local>
2020-04-21 08:23:46 -0500

    Intial UI Draft

---
+++ Find Car/Find Car/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find Car
+//
+//  Created by User on 4/21/20.
+//  Copyright © 2020 Abdikhaliq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
